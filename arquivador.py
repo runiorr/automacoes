b3192badc08bf9b684389f429171d8923144c6c1
José Rodrigues <69124149+runiorr@users.noreply.github.com>
2022-01-18 22:40:42 -0300

    Refatorado

--- arquivador.py
+++ arquivador.py
@@ -4,17 +4,18 @@ current_path = path.dirname(path.abspath(__file__)) + '/'
 
 (dirpath, dirnames, filenames) = next(walk(current_path)) 
 
-extensions = set(map(lambda file : file.split('.')[-1], filenames))
+extensions = set( map( lambda file : file[1] , map(path.splitext, filenames )))
 
 for ext in extensions:
     try:
         dir_name = current_path + ext + "_folder/"
         mkdir(dir_name)
+        print("Pasta "+ ext +" criada")
     except FileExistsError:
         print("Pasta "+ ext +" já existe")
 
 for file in filenames:
-    ext_folder = file.split('.')[1] + '_folder/'
+    ext_folder = "." + file.split('.')[-1] + '_folder/'
     if (file == "arquivador.py"):
         continue
     replace(current_path + file, current_path + ext_folder + file)
