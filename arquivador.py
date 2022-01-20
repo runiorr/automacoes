f47a1e96046f1f84c03e01804fc3d6fc8054d162
José Rodrigues <69124149+runiorr@users.noreply.github.com>
2022-01-19 21:15:18 -0300

    feat: flags para o terminal

--- arquivador.py
+++ arquivador.py
@@ -1,13 +1,30 @@
-import sys, time
-from os import walk, mkdir, path, replace
+#!/usr/bin/python3
+import sys, time, argparse
+from os import walk, mkdir, path, replace, getcwd
 
 start = time.time()
 
-current_path = path.dirname(path.abspath(__file__)) + '/'
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-p", "--path", dest="path", help="Pasta a ser organizada")
+parser.add_argument("-e", "--extension", dest="extension", help="Tipo dos arquivos")
+
+args = parser.parse_args()
+
+if(args.path):
+    if(args.path[-1] == '/'):
+        current_path = args.path
+    else:
+        current_path = args.path + '/'
+else:
+    current_path = getcwd() + '/'
 
 folder_files = next(walk(current_path))[-1]
 
-extensions = set([path.splitext(file)[-1] for file in folder_files if not (path.splitext(file)[-1] == '')])
+if(args.extension):
+    extensions = set(args.extension.split(','))
+else:
+    extensions = set([path.splitext(file)[-1].split('.')[1] for file in folder_files if not (path.splitext(file)[-1] == '')]) 
 
 if (len(folder_files) == 1):
     print("Nada para ser arquivado em {}".format(current_path))
@@ -15,30 +32,40 @@ if (len(folder_files) == 1):
 
 folder_name = lambda ext: current_path + ext + "_folder/"
 
+pastas = 0
+
 def make_dir(ext):
     try:
         if (ext == ''):
             return
         mkdir(folder_name(ext))
-        print("Pasta "+ ext +" criada")
+        global pastas
+        pastas += 1
     except FileExistsError:
-        print("Pasta "+ ext +" já existe")
+        return
 
 [make_dir(ext) for ext in extensions]
 
 arquivos = 0
 
+ignorados = 0
+
 def transfer(file):
-    if (len(file.split('.')) < 2 or file.split('.')[0] == ''):
+    try:
+        if (len(file.split('.')) < 2 or file.split('.')[0] == ''):
+            global ignorados
+            ignorados += 1
+            return
+        if (file == "file-organizer.py"):
+            return   
+        ext_folder = file.split('.')[-1] + '_folder/'
+        replace(current_path + file, current_path + ext_folder + file)
+        global arquivos
+        arquivos += 1
+    except:
         return
-    if (file == "arquivador.py"):
-        return
-    ext_folder = "." + file.split('.')[-1] + '_folder/'
-    replace(current_path + file, current_path + ext_folder + file)
-    global arquivos
-    arquivos += 1
 
 [transfer(file) for file in folder_files]
 
 end = time.time()
-print("\nO script demorou {} segundos para organizar {} arquivos\n".format(end - start, arquivos))
+print("\nO script demorou {:.5f} segundos\n{} arquivos transferidos\n{} pastas criadas\n{} arquivos ignorados\n".format(end - start, arquivos, pastas, ignorados))
